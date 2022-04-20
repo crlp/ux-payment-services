238b8ee914f893dee6a4d6f5032523e29757d5aa
crlp <rlozada1994gmail.com>
2022-04-20 01:55:41 -0500

    first commit

---
+++ src/main/java/bcp/bootcamp/uxpaymentservice/UxPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package bcp.bootcamp.uxpaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UxPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UxPaymentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bcp/bootcamp/uxpaymentservice/UxPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package bcp.bootcamp.uxpaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UxPaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
